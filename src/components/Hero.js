c5c3528b5c0d7412e342cfe2be7adf8bff6bfacd
saicoder33 <saidighe731@gmail.com>
2026-04-12 12:17:54 +0530

    Navbar updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,28 @@
 import React from "react";
+import "./hero.css";
+import saiImage from "./sai.jpg";
 
 function Hero() {
   return (
     <section id="hero" className="hero">
-      <h1>Hi, I'm Sai</h1>
-      <p>React Developer | Problem Solver | Innovator</p>
-      <a href="#projects" className="btn">View My Work</a>
+      <div className="info">
+        <div className="intro">
+          <h1>Hi, I'm Sai</h1>
+          <p>
+            I'm a passionate React developer with a knack for solving complex
+            problems and creating innovative solutions. I also enjoy mentoring
+            others and staying up-to-date with the latest industry trends.
+            React Developer | Problem Solver | Innovator
+          </p>
+          <button className="btn">
+            <a href="#projects" className="btn">View My Work</a>
+          </button>
+        </div>
+      </div>
+
+      <div className="hero-image">
+        <img src={saiImage} alt="Profile" />
+      </div>
     </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./navbar.css";
+
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2>My Portfolio</h2>
+       
+      <h2  className="title">My Portfolio</h2>
       <ul>
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a className="Link" href="#hero">Home</a></li>
+        <li ><a className="Link" href="#about">About</a></li>
+        <li><a className="Link" href="#projects">Projects</a></li>
+        <li ><a className="Link" href="#contact">Contact</a></li>
       </ul>
+      
     </nav>
   );
 }
